a05b47e49b3d68dfababe21a2ce882ef4902c17d
bwainwright72 <bw17311@my.bristol.ac.uk>
2020-01-26 13:48:48 +0000

    Referal data update

--- src/generate_referal_data.py
+++ src/generate_referal_data.py
@@ -11,14 +11,33 @@ __author__ = 'B Wainwright & D Jones'
 from format_data import *
 import urllib.request
 import json
+import re
+
+# Get the API Key
+def get_api_key():
+    file1 = open("api_key.txt","r")
+    api_key = file1.readline()
+    file1.close()
+    return api_key
 
 # TODO
 def generate_referal_data(all_services):
+    api_key = get_api_key()
 
-    res = urllib.request.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=SE1%208XX&destinations=B2%205NY").read()
-    data = json.loads(res.decode())
-    print(data)
-    #print(data["rows"][0]["elements"][0]["distance"])
+    for postcode in all_services:
+        source = re.sub(" ", "%20", postcode)
+        print(source)
+        input()
+        #source = "BS20%206AQ" #Formatted postcode
+
+        # Find nearest hospital
+        res = urllib.request.urlopen("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + source + "&destinations=BS1%203NU|BS10%205NB&key=" + api_key).read()
+        data = json.loads(res.decode())
+        d1 = data["rows"][0]["elements"][0]["distance"]["value"] #BS1 3NU
+        d2 = data["rows"][0]["elements"][1]["distance"]["value"] #BS10 5NB
+
+    print(d1)
+    print(d2)
     return None
 
 # TODO skeleton
