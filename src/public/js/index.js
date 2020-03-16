6abadedd65a65ff23a566fcb46233a32e77030b7
bwainwright72 <bw17311@my.bristol.ac.uk>
2020-03-16 16:59:45 +0000

    Map integrated

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -25,19 +25,26 @@ function load_vis_edges(svg, grid_ref) {
 
   promise
     .then(function() {
-        generate_edges(svg, x1s.length);
+        // generate_edges(svg, x1s.length);
+        //
+        // d3.selectAll("line")
+        //   .data(x1s)
+        //   .attr("x1", function(d) {return d;})
+        //   .data(y1s)
+        //   .attr("y1", function(d) {return d;})
+        //   .data(x2s)
+        //   .attr("x2", function(d) {return d;})
+        //   .data(y2s)
+        //   .attr("y2", function(d) {return d;})
+        //   .data(ts)
+        //   .style("stroke-width", function(d) {return d;});
 
-        d3.selectAll("line")
-          .data(x1s)
-          .attr("x1", function(d) {return d;})
-          .data(y1s)
-          .attr("y1", function(d) {return d;})
-          .data(x2s)
-          .attr("x2", function(d) {return d;})
-          .data(y2s)
-          .attr("y2", function(d) {return d;})
-          .data(ts)
-          .style("stroke-width", function(d) {return d;});
+        for (let i = 0; i < x1s.length; i++) {
+          var polyline = L.polyline([[x1s[i], y1s[i]],[x2s[i], y2s[i]]], {
+            color: 'red',
+            weight: ts[i] / 5
+          }).addTo(svg);
+        };
       })
     .catch(function() {
       console.log("Error Loading Edges");
@@ -63,26 +70,33 @@ function load_vis_nodes(svg, grid_ref) {
         services_name.push(data[i].name);
         services_appointments.push(data[i].appointments / 500);
       }
-
       resolve();
     });
   });
 
   promise
     .then(function() {
-      generate_nodes(svg, services_x.length);
+      // generate_nodes(svg, services_x.length);
+      //
+      // d3.selectAll("circle")
+      //   .data(services_x)
+      //   .attr("cx", function(d) {return d;})
+      //   .data(services_y)
+      //   .attr("cy", function(d) {return d;})
+      //   .data(services_appointments)
+      //   .attr("r", function(d) {return d;})
+      //   .data(services_name)
+      //   .attr('id', function(d) {return d;});
+      for (let i = 0;i<services_x.length;i++) {
+        var circle = L.circle([services_x[i], services_y[i]], {
+            color: 'none',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: services_appointments[i] * 25
+          }).addTo(svg);
+      };
 
-      d3.selectAll("circle")
-        .data(services_x)
-        .attr("cx", function(d) {return d;})
-        .data(services_y)
-        .attr("cy", function(d) {return d;})
-        .data(services_appointments)
-        .attr("r", function(d) {return d;})
-        .data(services_name)
-        .attr('id', function(d) {return d;});
-
-      d3.selectAll("#GP").attr("fill", "rgb(244,89,91)");
+      //d3.selectAll("#GP").attr("fill", "rgb(244,89,91)");
     })
     .catch(function() {
       console.log("Error Loading Nodes");
@@ -94,9 +108,9 @@ function load_vis_nodes(svg, grid_ref) {
 // Loads postcode to co-ordinate data
 function load_grid_ref() {
   var grid_ref = {};
-  d3.csv("/js/postcode_xy.csv", function(data) {
+  d3.csv("/js/postcode_ne.csv", function(data) {
     for (let i = 0; i < data.length; i++) {
-      grid_ref[data[i].postcode] = [data[i].x, data[i].y];
+      grid_ref[data[i].postcode] = [data[i].north, data[i].east];
     };
   });
 
@@ -144,34 +158,34 @@ function create_canvas(width, height) {
 // Manage workflow
 window.onload = function() {
 
-    // Load Data Vis from data
-    var svg = create_canvas(750, 750);
-    var grid_ref = load_grid_ref();
-    var referrals_edges = load_vis_edges(svg, grid_ref);
-    var services_nodes = load_vis_nodes(svg, grid_ref);
-
     // initialize the map on the "map" div with a given center and zoom
     var mymap = L.map('mapid', {
         center: [51.455, -2.599],
         zoom: 13
     });
 
+    // Load map of Bristol
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
-      accessToken: '<ACCESS TOKEN HERE>'
+      accessToken: 'pk.eyJ1IjoicGF2ZS1oZWFsdGgiLCJhIjoiY2s3dWQxZHUzMTZlYTNncXR1OHB2NTBkYiJ9.7mf8ut1FJpHCFzcsy7qiDA'
     }).addTo(mymap);
 
-    var circle = L.circle([51.455, -2.599], {
-      color: 'none',
-      fillColor: '#f03',
-      fillOpacity: 0.5,
-      radius: 500
-    }).addTo(mymap);
+    // var circle = L.circle([51.455, -2.599], {
+    //   color: 'none',
+    //   fillColor: '#f03',
+    //   fillOpacity: 0.5,
+    //   radius: 500
+    // }).addTo(mymap);
 
-    var polyline = L.polyline([[51.455, -2.599],[51,-2]], {color: 'red'}).addTo(mymap);
+    //var polyline = L.polyline([[51.455, -2.599],[51,-2]], {color: 'red'}).addTo(mymap);
 
+    // Load Data Vis from data
+    //var svg = create_canvas(750, 750);
+    var grid_ref = load_grid_ref();
+    var referrals_edges = load_vis_edges(mymap, grid_ref);
+    var services_nodes = load_vis_nodes(mymap, grid_ref);
 };
