6b0a4007f4038d7367e34eeec0ba1e60c4938d71
Ben <bw17311@bristol.ac.uk>
2021-03-14 10:44:40 +0000

    Merged with dev

---
+++ src/public/capacity-modelling/js/index.js
@@ -0,0 +1,83 @@
+"use strict";
+
+import {load_vis_nodes} from './vis_nodes.js';
+import {load_data_from_default, load_data_from_user} from './data_input_handler.js';
+
+// Global variables
+var slider = document.getElementById("daypicker");
+var dataset_upload = document.getElementById("dataset_upload");
+var mymap;
+var earth;
+var services_nodes;
+
+// Manage workflow
+window.onload = function() {
+
+    // initialize the map on the "map" div with a given center and zoom
+    mymap = L.map('mapid', {
+        center: [51.455, -2.599],
+        zoom: 13
+    });
+
+    // Load map of Bristol
+    earth = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoicGF2ZS1oZWFsdGgiLCJhIjoiY2s3dWQxZHUzMTZlYTNncXR1OHB2NTBkYiJ9.7mf8ut1FJpHCFzcsy7qiDA'
+    });
+
+
+    // Load Data Vis from data
+    services_nodes = load_data_from_default();
+    setTimeout(function() {load_vis_nodes(mymap, services_nodes[0]); document.getElementById("datadate").innerHTML = services_nodes[0].date;}, 3000);
+    earth.addTo(mymap); // ToDo fix error on this line with Express Server
+};
+
+// Load all data from csv files
+async function load_csv_files() {
+  var services = [];
+  slider.value = 0;
+  slider.max = dataset_upload.files.length - 1;
+
+  for (let i = 0; i < dataset_upload.files.length; i++) {
+    var reader = new FileReader();
+    var file = dataset_upload.files[i];
+
+    reader.onload = async function() {
+      var service = await load_data_from_user(reader.result, dataset_upload.files[i].name.replace(/\.csv/g, ''));
+      services.push(service);
+    };
+    reader.onerror = error => reject(error)
+    reader.readAsText(file);
+  };
+
+  services_nodes = services;
+};
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  mymap.eachLayer(function (layer) {
+    mymap.removeLayer(layer);
+  });
+  earth.addTo(mymap);
+  load_vis_nodes(mymap, services_nodes[this.value]);
+  document.getElementById("datadate").innerHTML = services_nodes[this.value].date;
+};
+
+// File Upload handler
+dataset_upload.onchange = async function () {
+  services_nodes = [];
+
+  // Load files into services_nodes
+  await load_csv_files();
+
+  // Redraw map
+  mymap.eachLayer(function (layer) {
+    mymap.removeLayer(layer);
+  });
+  earth.addTo(mymap);
+  setTimeout(function() {load_vis_nodes(mymap, services_nodes[0]); document.getElementById("datadate").innerHTML = services_nodes[0].date;}, 3000);
+};
---
+++ src/public/capacity-modelling/js/vis_nodes.js
@@ -0,0 +1,68 @@
+// Load data from csv to create nodes
+export function load_vis_nodes(svg, services_nodes) {
+
+  for (let i = 0; i < services_nodes.location.length; i++) {
+    // Check data is present
+    if (typeof services_nodes.x[i] == 'undefined') {continue;};
+    if (typeof services_nodes.y[i] == 'undefined') {continue;};
+    if (typeof services_nodes.appointments[i] == 'undefined') {continue;};
+    if (typeof services_nodes.capacity[i] == 'undefined') {continue;};
+
+    // Draw capacity
+    var circle_capacity = L.circle([services_nodes.x[i], services_nodes.y[i]], {
+        color: 'black',
+        weight: '0.5',
+        fillColor: '#1e88e5',
+        fillOpacity: 0.7,
+        radius: services_nodes.capacity[i] / 20
+    }).addTo(svg);
+
+    // Draw node
+    var circle_usage = L.circle([services_nodes.x[i], services_nodes.y[i]], {
+        color: 'black',
+        weight: '0.5',
+        fillColor: '#ffc107',
+        fillOpacity: 0.7,
+        radius: services_nodes.appointments[i] / 20
+    }).addTo(svg);
+
+
+    // Create string from metadata
+    var metadata_string = '<div id="metadata" style="display:block">+ Location: ' + services_nodes.location[i];
+    for (let j = 0; j < services_nodes.metadata[i].length; j++) {
+      metadata_string = metadata_string + "<br></br>   + "
+        + services_nodes.metadata[i][j][0] + ": "
+        + services_nodes.metadata[i][j][1];
+    };
+    metadata_string = metadata_string + "</div>";
+
+    // Add extra information popup
+    circle_usage.on('mouseover', function (event) {
+      var info_popup = L.popup()
+       .setLatLng(event.latlng)
+       .setContent('<br></br><b>Name:</b> ' + services_nodes.name[i]
+          + '<br></br><b>Appointments:</b> ' + services_nodes.appointments[i]
+          + '<br></br><b>Capacity:</b> ' + services_nodes.capacity[i]
+          + '<br></br><br></br><div id="meta_click"><b>Extra Information:</b></div>' + metadata_string)
+       .openOn(svg);
+
+       // document.getElementById("meta_click").addEventListener('click', toggle_meta);
+    });
+
+  };
+
+  return;
+};
+
+function toggle_meta() {
+  var list = document.getElementById("metadata");
+  var head = document.getElementById("meta_click");
+  console.log(list.style.display);
+  if (list.style.display == "none"){
+      list.style.display = "block";
+      head.innerHTML = "<b>see less...</b>";
+  } else {
+      list.style.display = "none";
+      head.innerHTML = "<b>see more...</b>";
+  }
+}
